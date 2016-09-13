5499facf0e3e04170b7979a601a926258b98571a
cory olson and noah yasskin <cory@>
2016-09-13 11:20:06 -0700

    unmigrated Devise password authentication and set up new bCrypt authentication with forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :questions
   has_many :votes
   has_many :answers
-  has_secure_password
 
+  has_secure_password
+  validates :password, :presence => true
   validates :name, :presence => true
 end
